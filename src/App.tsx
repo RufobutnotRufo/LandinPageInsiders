ee8a0849d1e652891a4a956fb97fd4f4cefaa960
Rufat <mrufat755@gmail.com>
2024-10-24 15:07:47 +0400

    add-header-component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./Global.css";
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Header/Header.tsx
@@ -0,0 +1,42 @@
+import "./Header.scss";
+import LogoSvg from "../Header/logo.svg";
+
+const Header: React.FC = () => {
+  const headerMainMenuItems: any = [
+    "Home",
+    "Success Stories",
+    "Services",
+    "Blog",
+    "About us",
+    "Career",
+  ];
+
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="header-inner">
+          <div className="logo">
+            <img className="logo-img" src={LogoSvg} />
+          </div>
+
+          <div className="hader-nav-menu">
+            <ul className="header-nav-menu-items">
+              {headerMainMenuItems.map((item: any) => (
+                <li className="header-nav-menu-item">
+                  <a className="header-nav-menu-item-link">{item}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="user-emenet">
+            <button className="sign">Sign Up</button>|
+            <button className="sign">Sign In</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
